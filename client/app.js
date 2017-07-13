5a7e1472fa0eaa0537e7097e5ce8a5ffe70cfda5
mholliday6611 <mholliday6611@gmail.com>
2017-07-13 12:21:49 -0400

    instagram

--- client/app.js
+++ client/app.js
@@ -1,4 +1,39 @@
-	angular.module('Instagram',['ngRoute', 'ngMessages'])
-		.config(function($routeProvider){
+	angular.module('Instagram',['ngRoute', 'ngMessages', 'satellizer'])
+		.config(function($routeProvider, $authProvider){
+			$routeProvider
+			  .when('/', {
+			    templateUrl: 'views/home.html',
+			    controller: 'HomeCtrl'
+			  })
+			  .when('/login', {
+			    templateUrl: 'views/login.html',
+			    controller: 'LoginCtrl'
+			  })
+			  .when('/signup', {
+			    templateUrl: 'views/signup.html',
+			    controller: 'SignupCtrl'
+			  })
+			  .when('/photo/:id', {
+			    templateUrl: 'views/detail.html',
+			    controller: 'DetailCtrl'
+			  })
+			  .otherwise('/');
+			$authProvider.loginUrl = 'http://localhost:3000/auth/login';
+			$authProvider.signupUrl = 'http://localhost:3000/auth/signup';
+			$authProvider.oauth2({
+			  name: 'instagram',
+			  url: 'http://localhost:3000/auth/instagram',
+			  redirectUri: 'http://localhost:8000',
+			  clientId: '649ff99ceaca4ef89d15bc3b8ec5545f',
+			  requiredUrlParams: ['scope'],
+			  scope: ['likes'],
+			  scopeDelimiter: '+',
+			  authorizationEndpoint: 'https://api.instagram.com/oauth/authorize'
+			});
 
-		});
+		})
+		.run(function($rootScope, $window, $auth) {
+  if ($auth.isAuthenticated()) {
+    $rootScope.currentUser = JSON.parse($window.localStorage.currentUser);
+  }
+});
---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	db: process.env.db || 'localhost',
+	clientSecret: process.env.clientSecret || 'a47b0a6bf4ba4c0691fe1645ab0e4581',
+	tokenSecret: process.env.tokenSecret || 'blahblah'
+};
